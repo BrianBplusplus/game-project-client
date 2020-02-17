8b8050c49a3c34eb8da1d07860c9cc60081b4e82
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-17 12:18:10 +0100

    implemented basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
+
+import { Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
+
+import Testcomponent from "./components/Testcomponent";
+import Routecomponent from "./components/Routecomponent";
 
 function App() {
   return (
     <Provider store={store}>
       <h1>Hola</h1>
+      <Testcomponent />
+      <Route path="/router" exact component={Routecomponent} />
     </Provider>
   );
 }
---
+++ src/components/Routecomponent.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Routecomponent extends Component {
+  render() {
+    return <div>This is a route component</div>;
+  }
+}
