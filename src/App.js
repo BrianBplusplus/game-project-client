71ed62add94329b604f537b142d84465390c9db3
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-17 17:35:16 +0100

    added stream

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
 
 import { Route } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -8,14 +9,30 @@ import store from "./store/store";
 import LoginScreenContainer from "./components/Login/LoginScreenContainer";
 import SignupScreenContainer from "./components/Signup/SignupScreenContainer";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <h1>A FAKE ARTIST GOES TO NEW YORK</h1>
-      <Route path="/signup" exact component={SignupScreenContainer} />
-      <Route path="/" exact component={LoginScreenContainer} />
-    </Provider>
-  );
+class App extends React.Component {
+  stream = new EventSource("http://localhost:4000/stream");
+
+  componentDidMount() {
+    this.stream.onmessage = event => {
+      const { data } = event;
+
+      const action = JSON.parse(data);
+
+      const { type, payload } = action;
+
+      console.log("stream action", action);
+    };
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <h1>A FAKE ARTIST GOES TO NEW YORK</h1>
+        <Route path="/signup" exact component={SignupScreenContainer} />
+        <Route path="/" exact component={LoginScreenContainer} />
+      </Provider>
+    );
+  }
 }
 
 export default App;
