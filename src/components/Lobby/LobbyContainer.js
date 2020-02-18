f818e65a8d509cdb81c065a9ba052fca398a7d4e
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-18 11:22:01 +0100

    moved stream from lobby to app

--- src/components/Lobby/LobbyContainer.js
+++ src/components/Lobby/LobbyContainer.js
@@ -5,23 +5,6 @@ import { Link } from "react-router-dom";
 import Room from "./Room";
 
 export class LobbyContainer extends Component {
-  stream = new EventSource(
-    "https://game-project-alex-brian-server.herokuapp.com/stream"
-  );
-
-  componentDidMount() {
-    this.stream.onmessage = event => {
-      const { data } = event;
-
-      const action = JSON.parse(data);
-
-      const { type, payload } = action;
-
-      console.log("stream action", action);
-
-      this.props.dispatch(action);
-    };
-  }
   render() {
     console.log("this.props", this.props);
     return (
