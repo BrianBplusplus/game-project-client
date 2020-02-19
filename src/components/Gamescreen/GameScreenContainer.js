6afccb176477c62ef2a7928938d3c8126e273790
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-19 15:40:29 +0100

    added map functionality

--- src/components/Gamescreen/GameScreenContainer.js
+++ src/components/Gamescreen/GameScreenContainer.js
@@ -9,21 +9,32 @@ import Bottombar from "./Bottombar";
 export class GameScreenContainer extends Component {
   render() {
     const roomId = parseInt(this.props.match.params.roomId);
-
+    console.log("this.props.rooms", this.props.rooms);
     return (
       <div>
-        <h2>
-          {this.props.rooms &&
-            this.props.rooms.map(room => {
-              if (room.id === roomId) {
-                return <p>{room.name}</p>;
-              }
-            })}
-        </h2>
-        <div className="gamescreencontainer">
-          <Chatroom />
-          <GameScreen />
-        </div>
+        {this.props.rooms &&
+          this.props.rooms.map(room => {
+            if (room.id === roomId) {
+              return (
+                <div key={room.id}>
+                  <h2>{room.name}</h2>
+
+                  <ul>
+                    Players:
+                    {room.users.map(user => {
+                      return <li>{user.userName}</li>;
+                    })}
+                  </ul>
+
+                  <div className="gamescreencontainer">
+                    <Chatroom />
+                    <GameScreen />
+                  </div>
+                </div>
+              );
+            }
+          })}
+
         <Bottombar />
       </div>
     );
