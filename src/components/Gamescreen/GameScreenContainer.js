221855cf4a69d51515d8875c229c7924738d837a
avizmarques <aviz.marques@gmail.com>
2020-02-20 12:45:32 +0100

    receive drawing data and display it

--- src/components/Gamescreen/GameScreenContainer.js
+++ src/components/Gamescreen/GameScreenContainer.js
@@ -5,15 +5,58 @@ import "./game.css";
 import Chatroom from "./Chatroom";
 import GameScreen from "./GameScreen";
 import Bottombar from "./Bottombar";
+import axios from "axios";
+import P5Wrapper from "react-p5-wrapper";
 
 export class GameScreenContainer extends Component {
+  roomId = parseInt(this.props.match.params.roomId);
+
+  sketch = p => {
+    let canvas;
+
+    p.setup = () => {
+      canvas = p.createCanvas(700, 400);
+      p.background(255);
+    };
+
+    p.myCustomRedrawAccordingToNewPropsHandler = props => {
+      console.log(props.roomInfo.drawingLines);
+      const allLines = props.roomInfo.drawingLines;
+      if (allLines && allLines.length) {
+        allLines.map(line => line.data.map(valuePair => newDrawing(valuePair)));
+      }
+    };
+
+    p.mouseDragged = () => {
+      p.fill(51, 255, 177);
+      p.noStroke();
+      p.square(p.mouseX, p.mouseY, 10);
+
+      let data = [p.mouseX, p.mouseY];
+
+      mouseData.push(data);
+    };
+
+    const mouseData = [];
+
+    p.mouseReleased = async () => {
+      const url = `https://game-project-alex-brian-server.herokuapp.com/drawing`;
+      await axios.post(url, { data: mouseData, roomId: this.roomId });
+    };
+
+    const newDrawing = data => {
+      p.noStroke();
+      p.fill(51, 255, 177);
+      p.square(data[0], data[1], 10);
+    };
+  };
+
   render() {
-    const roomId = parseInt(this.props.match.params.roomId);
     return (
       <div>
         {this.props.rooms &&
           this.props.rooms.map(room => {
-            if (room.id === roomId) {
+            if (room.id === this.roomId) {
               return (
                 <div key={room.id}>
                   <h2>{room.name}</h2>
@@ -34,8 +77,12 @@ export class GameScreenContainer extends Component {
                         })}
                       />
                     }
-
-                    <GameScreen />
+                    <P5Wrapper
+                      sketch={this.sketch}
+                      roomInfo={this.props.rooms.find(
+                        room => room.id === this.roomId
+                      )}
+                    />
                   </div>
                 </div>
               );
